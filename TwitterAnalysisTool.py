7d1386e007d0c63326ed2a11cab08f737f32c3da
dylan-gregory-1011 <38666413+dylan-gregory-1011@users.noreply.github.com>
2018-11-01 15:17:37 -0500

    Update TwitterAnalysisTool.py

--- TwitterAnalysisTool.py
+++ TwitterAnalysisTool.py
@@ -4,7 +4,8 @@ Twitter Sentiment Analysis Project: Utilizes three distinct objects in order to 
 from the Twitter Search API, cleanse the data so that it can be stored properly, and apply a sentiment tool
 to the tweets to try to gauge the daily sentiment for each group.
 
-:: param ProjectName: The project name that you want to run.
+::param ProjectGroup: The group that the project is to be run in 
+::param ProjectName: Sub name of the project.
 
 Version 1.0: Initial Scope of the project.  This includes the objects that perform the ETL and analysis
 as well as the driver program (This file).
@@ -19,18 +20,19 @@ Version 2.0:
     - Reformatted the query data to be saved in a json file
     - Created the TwitterAnalysisTool class to drive the daily processing of twitter activity as well
         as wrapped the process in a bash script so that multiple projects can be run at once
-
+Version 3.0:
+    -Updated to fit within a structure that allowed multiple projects to be used
+    -Inserted cleansed data into an SQLite database.  Also tried to trim the amouht of distinct files
 """
-
 #Imports
 import sys
 from pathlib import Path
-from TwitterScraper import TwitterScraper
-from TwitterCleanser import TwitterCleanser
-from TwitterSentimentAnalyzer import TwitterSentimentAnalyzer
-from multiprocessing import Pool
+from PythonDataModules.TwitterScraper import TwitterScraper
+from PythonDataModules.TwitterCleanser import TwitterCleanser
+from PythonDataModules.TwitterSentimentAnalyzer import TwitterSentimentAnalyzer
 import logging
 logging.basicConfig(stream=sys.stdout, level = logging.INFO)
+import sqlite3 as db
 import json
 
 __author__ = "Dylan Smith"
@@ -38,7 +40,7 @@ __copyright__ = "Copyright (C) 2018 Dylan Smith"
 __credits__ = ["Dylan Smith"]
 
 __license__ = "Personal Use"
-__version__ = "2.0"
+__version__ = "3.0"
 __maintainer__ = "Dylan Smith"
 __email__ = "-"
 __status__ = "Development"
@@ -48,21 +50,24 @@ class TwitterAnalysisTool(object):
     curr_dir = Path(__file__).resolve().parent
 
     #initialization
-    def __init__(self, project_name):
-        self.prj_nm = project_name
+    def __init__(self, project_area, project_name):
+        self.proj_nm = project_name
+        self.proj_data_dir = self.curr_dir.joinpath('DataSources','Twitter', project_name)
+        self.proj_analysis_dir = self.curr_dir.joinpath(project_area, project_name)
+        self.api_key = self.curr_dir.joinpath('APIKeys', project_name + 'TwitterAPIKeys.json')
+        self.connection = db.connect(str(self.proj_analysis_dir.joinpath('CleansedData.db')))
 
     def downloadRecentTwitterActivity(self):
         """
         This function updates the twitter dataset and downloads all relevant Tweets for each group that is part of
         the project.  Twitter API specifications are set in this function so that the parameters outside of the query
         terms are all the same
-
-        ::params - project_name: takes a string input for the project name and accesses the correct folder as well as the
-                                 inputs the data into the correct data folders
         """
-        self.twython_scraper = TwitterScraper(project_name = self.prj_nm)
-
-        qry_file = self.twython_scraper.returnQueriesToRun()
+        twython_scraper = TwitterScraper(proj_data_dir = self.proj_data_dir,
+                                        proj_analysis_dir = self.proj_analysis_dir,
+                                        api_key_file = self.api_key)
+        logging.info('-- Twython object instantiated for %s--' % self.proj_nm)
+        qry_file = twython_scraper.returnQueriesToRun()
         with open(qry_file, "r") as file:
              prj_qry_data = json.load(file)
 
@@ -74,7 +79,7 @@ class TwitterAnalysisTool(object):
             grp_qry["q"] = grp_data["Query"]
 
             #download tweets and then update the json file that drives the project
-            new_max, last_updt = self.twython_scraper.downloadHistoricalTweets(grp_nm, grp_qry, grp_data['MaxRecord'])
+            new_max, last_updt = twython_scraper.downloadHistoricalTweets(grp_nm, grp_qry, grp_data['MaxRecord'])
             prj_qry_data["GroupQueries"][grp_nm]['MaxRecord'] = new_max
             prj_qry_data["GroupQueries"][grp_nm]['LastUpdate'] = last_updt
 
@@ -90,11 +95,12 @@ class TwitterAnalysisTool(object):
         params load_type: Takes a string input and specifies which records should be classified.  It is either
                           a delta load or a full load.
         """
-        self.twitter_cleanser = TwitterCleanser(self.prj_nm, load_type)
+        self.twitter_cleanser = TwitterCleanser(proj_data_dir = self.proj_data_dir
+                                                ,db_connection= self.connection
+                                                ,load_type = load_type)
         self.twitter_cleanser.uploadTweetsIntoCleanser()
         self.twitter_cleanser.updateOriginalTweets()
         self.twitter_cleanser.cleanseRepliedTweets()
-        self.twitter_cleanser.sumRetweets()
 
     def calculateSentiment(self):
         """
@@ -102,29 +108,35 @@ class TwitterAnalysisTool(object):
         is set by the datesToUpdate field that is specified below.  If no dates are specified then the full run
         is calculated
         """
-        self.datesToUpdate = self.twitter_cleanser.getDeltaDatesToUpdate()
-        self.twitter_sentiment = TwitterSentimentAnalyzer(self.prj_nm, self.datesToUpdate)
-        self.twitter_sentiment.calculateSentimentForTweets()
+        twitter_sentiment = TwitterSentimentAnalyzer(proj_data_dir = self.proj_data_dir
+                                                ,proj_analysis_dir = self.proj_analysis_dir
+                                                ,db_connection = self.connection
+                                                ,days_to_update = self.twitter_cleanser.getDaysToUpdate())
+        twitter_sentiment.calculateSentimentForTweets()
 
     def createInitialDirectories():
-        prj_data_path = Path(__file__).resolve().parent.joinpath(project_name, 'Data')
+        prj_data_path = Path(__file__).resolve().parent.joinpath(project_area ,project_name, 'Data')
         for grp_nm, grp_data in prj_qry_data["GroupQueries"].items():
             prj_data_path.joinpath(grp_nm, "RawData").mkdir(parents = True)
             prj_data_path.joinpath(grp_nm, "CleansedData").mkdir(parents = True)
         return None
 
-def main(project_name):
+def main(project_area, project_name):
     """
     The driver function for the TwitterAnalysisTool.  This instantiates an object for each project and goes through
     and downloads the recent twitter activity, processes and stores the data, and then calculates the sentiment for the data
+    ::param project_area: The project area that is being processed. This corresponds to the folder that will be used
+    ::param project_name: The project name that will be used to drive the Analysis
     """
-    twitter_analysis = TwitterAnalysisTool(project_name=project_name)
+    twitter_analysis = TwitterAnalysisTool(project_area=project_area,
+                                            project_name=project_name)
     twitter_analysis.downloadRecentTwitterActivity()
-    twitter_analysis.processAndStoreData('DELTA')
+    twitter_analysis.processAndStoreData('FULL')
     twitter_analysis.calculateSentiment()
     print('Download for %s has completed' % project_name)
 
 
 if __name__ == '__main__':
     #Run the process for the project in question
-    main(sys.argv[1])
+    #sample statement to run >>python3 TwitterAnalysisTool.py SportsSentiment NFL
+    main(sys.argv[1], sys.argv[2])
